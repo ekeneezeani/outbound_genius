164608ee6cdfc68f9f50d4d9c5853b2de809d0c7
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2023-11-30 11:44:55 -0600

    changed swtup to vite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
-import MyButton from './MyButton';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <MyButton/>
-    </div>
-  );
+    <>
+      <h1>HELLO</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Login.tsx
@@ -0,0 +1,15 @@
+
+
+const Login = () => {
+
+  return (
+    <>
+      <form>
+        <input type="email" placeholder="email"/>
+        <input type="password" placeholder="password"/>
+      </form>
+    </>
+  )
+}
+
+export default Login
